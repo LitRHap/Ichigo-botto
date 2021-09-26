92eb0cf8c33f0529f4798a735ba5d8d5243592da
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-09-27 00:57:52 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,9 +1,6 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import request from '../../lib/request'
-import { MessageType } from '@adiwajshing/baileys'
-
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
@@ -13,7 +10,6 @@ export default class Command extends BaseCommand {
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            dm: true,
             aliases: ['h']
         })
     }
@@ -25,27 +21,24 @@ export default class Command extends BaseCommand {
             for (const command of commands) {
                 const info = this.handler.commands.get(command)
                 if (!command) continue
-                if (!info?.config?.category || info.config.category === 'dev') continue
+                // if (!info?.config?.category || info.config.category === 'dev') continue
+                if (!info?.config?.category) continue
                 if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
                 else {
                     categories[info.config.category] = []
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `✨ Ichigo's Command List ✨\n\n`
+            let text = `👾 *Ichigo's Command List* 👾\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}\n❐ \`\`\`${categories[
+                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
                     key
                 ]
                     .map((command) => command.config?.command)
                     .join(', ')}\`\`\`\n\n`
-            return void M.reply( await request.buffer('https://images6.alphacoders.com/903/903824.png'),  
-                                MessageType.image,            
-                                undefined,
-                                undefined,
-
-                `${text} 🗃️ Note: Use ${this.client.config.prefix}help <command_name> to view the command info`
+            return void M.reply(
+                `${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
             )
         }
         const key = parsedArgs.joined.toLowerCase()
@@ -53,17 +46,17 @@ export default class Command extends BaseCommand {
         if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
         const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
         M.reply(
-            `🎫 Command: ${this.client.util.capitalize(command.config?.command)}\n🎗️ Status: ${
+            `🎫 *Command:* ${this.client.util.capitalize(command.config?.command)}\n🎗️ *Status:* ${
                 state ? 'Disabled' : 'Available'
-            }\n🀄 Category: ${this.client.util.capitalize(command.config?.category || '')}${
+            }\n🀄 *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
                 command.config.aliases
-                    ? `\n🍥 Aliases: ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    ? `\n🍥 *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
                     : ''
-            }\n🃏 Group Only: ${this.client.util.capitalize(
+            }\n🃏 *Group Only:* ${this.client.util.capitalize(
                 JSON.stringify(!command.config.dm ?? true)
-            )}\n🎀 Usage: ${command.config?.usage || ''}\n\n🔖 Description: ${command.config?.description || ''}`
+            )}\n🎀 *Usage:* ${command.config?.usage || ''}\n\n🔖 *Description:* ${command.config?.description || ''}`
         )
     }
 
-    emojis = ['🧣', '🌀', '💫', '🎬', '👑', '🎈', '🎭', '🎲', '🍀']
+    emojis = ['👨‍💻', '🌀', '🎴', '🔮', '👑', '🎈', '⚙️', '⚙️', '🍀']
 }
