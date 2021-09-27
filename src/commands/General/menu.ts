e7ab15e3040b47d1d44c4d73b5d487019ccbb1eb
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-09-28 02:49:33 +0530

    Update menu.ts

--- src/commands/General/menu.ts
+++ src/commands/General/menu.ts
@@ -1,1 +1,29 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '',
+         
+
+        })
+    }
+
+
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+
+
+
+return void M.reply(await request.buffer('https://c.tenor.com/LtTgb4O_hjMAAAAC/darling-in-the-franxx-anime.gif'),
+MessageType.image,
+Mimetype.gif,                    
+            undefined,
+            undefined,
+           `*Did you mean : €help*`
