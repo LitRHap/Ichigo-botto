b4d4c453389cbd92716e7c3caf186e0a110268c5
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-07 15:59:09 +0530

    Update menu.ts

--- src/commands/General/menu.ts
+++ src/commands/General/menu.ts
@@ -105,7 +105,7 @@ Use =help <command_name> to view the command info
 `
         return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption:mn,quoted:M }
+            caption:mn }
         )
     }
 }
