7cf69853940cecb3ebd6ec07040494e51767354e
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-07 21:18:58 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -35,7 +35,7 @@ export default class Command extends BaseCommand {
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👋🏻Konichiwa! *${M.sender.username}*, I'm Ichigo.\n\nThe commands are listed below .\n\n`
+            let text = `👋🏻Konichiwa! *${M.sender.username}*, I'm Nezuko Chan.\n\nThe commands are listed below .\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
