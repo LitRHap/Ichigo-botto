4beed3c5d52e70244121a880e84d2faec1dbb807
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-07 15:22:04 +0530

    Update menu.ts

--- src/commands/General/menu.ts
+++ src/commands/General/menu.ts
@@ -9,6 +9,10 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             command: 'help',
+            description: 'Displays the help menu or shows the info of the command provided',
+            category: 'general',
+            usage: `${client.config.prefix}help (command_name)`,
+            aliases: ['h']
         })
     }
 
