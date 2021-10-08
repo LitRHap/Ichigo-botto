4102aa502b8b372e56aff178619e0a63411f0b55
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-09 03:23:50 +0530

    Update Quote.ts

--- src/commands/Fun/Quote.ts
+++ src/commands/Fun/Quote.ts
@@ -16,12 +16,23 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+     const  MessageType.buttonsMessage  = (type === 'conversation') ? M.message.conversation : (type === 'buttonsMessage') ? M.message.extendedTextMessage.text : ''
+     const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
+     MessageType.buttonsMessage = command
         await axios
             .get(`https://api.quotable.io/random`)
             .then((response) => {
                 // console.log(response);
                 const text = `📝 *Content:* ${response.data.content}\n\n*✍️ Author:* ${response.data.author}`
-                M.reply(text)
+                const buttonsk = [
+    {buttonId: '${this.client.config.prefix}why', buttonText: {displayText: ':next'}, type: 1}]
+                const buttonMessagek = {
+      contentText: text
+      footerText: 'Ichigo-botto',
+      buttons: buttonsk,
+      headerType:1
+                }
+                return void this.client.sendMessage(M.from,text,buttonsk,MessageType.buttonMessage,{quoted:M.WAMessage})
             })
             .catch((err) => {
                 M.reply(`🔍 Error: ${err}`)
