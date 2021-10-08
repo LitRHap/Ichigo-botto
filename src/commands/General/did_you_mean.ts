e10176b4696a4f25c6a5c8b882133012c7bcc1e6
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-08 17:06:12 +0530

    Update did_you_mean.ts

--- src/commands/General/did_you_mean.ts
+++ src/commands/General/did_you_mean.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
             './assets/images/ichi/nezuko.mp4'
         ]
         let chitoge = n[Math.floor(Math.random() * n.length)]
-        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage
             mimetype: Mimetype.gif,
             caption: `Do you mean *${this.client.config.prefix}help*? \n`}
         )
