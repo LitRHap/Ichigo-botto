3001bd80feab931358d5aadbe8abc0a059a73c54
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-09 01:23:50 +0530

    Update pick.ts

--- src/commands/Fun/pick.ts
+++ src/commands/Fun/pick.ts
@@ -13,17 +13,23 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (args.length < 1) return reply(from, ':pick @_', {quoted:mek});
-                      membr = []
-                      txt=args.join(' ')
-                      const mes = groupMembers
+        const args = body.trim().split(/ +/).slice(1)
+        const groupMembers = isGroup ? groupMetadata.participants : ''
+        const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+        const mentions = (teks, memberr, id) => {
+        (id == null || id == undefined || id == false) ? return void this.client.sendMessage(M.from, teks.trim(), extendedText, {contextInfo: {"mentionedJid": memberr}}) :  return void this.client.sendMessage(M.from, teks.trim(), extendedText, {quoted: mek, contextInfo: {"mentionedJid": memberr}})
+      }
+        if (args.length < 1) return M.reply(M.from,':pick @_', {quoted:M.WAMessage});
+                     var membr = []
+                      var txt=args.join(' ')
+                      const mes = groupMembers.
                       const msk = groupMembers
                       const siaps = mes[Math.floor(Math.random() * mes.length)]
                       const sips = pushname[Math.floor(Math.random() * msk.length)]
                       teks = `*${txt} in this group:* *@${siaps.jid.split('@')[0]}*`
                       membr.push(siaps.jid)
                       mentions(teks, membr, true)
-                      break
+                     
                     
      }
 }
