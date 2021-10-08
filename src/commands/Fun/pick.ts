4bcbf8233fb0b387d5b7695ec03a626cd53cfd6e
Pratyush <86536877+pratyush4932@users.noreply.github.com>
2021-10-09 00:51:44 +0530

    Update pick.ts

--- src/commands/Fun/pick.ts
+++ src/commands/Fun/pick.ts
@@ -1,1 +1,28 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '',
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (args.length < 1) return reply(from, ':pick @_', {quoted:mek});
+                      membr = []
+                      txt=args.join(' ')
+                      const mes = groupMembers
+                      const msk = groupMembers
+                      const siaps = mes[Math.floor(Math.random() * mes.length)]
+                      const sips = pushname[Math.floor(Math.random() * msk.length)]
+                      teks = `*${txt} in this group:* *@${siaps.jid.split('@')[0]}*`
+                      membr.push(siaps.jid)
+                      mentions(teks, membr, true)
+                      break
+                    
+     }
